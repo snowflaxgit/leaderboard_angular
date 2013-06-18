2b46a7bd0ea4b26b62927740aec52acdb8424a38
Krunal <krunal@snowflax.com>
2013-06-18 17:31:41 +0530

    Socket Auto Reconnect

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,10 @@
 // JavaScript Document
 var myApp = angular.module('myApp', []);
-var socket = io.connect(); // socket connection 	
+var socket = io.connect('http://178.79.147.154:3660', {
+        'reconnect': true,
+        'reconnection delay': 500,
+        'max reconnection attempts': 50
+    }); // socket connection 	
 
 function LeaderBoardCtrl($scope, $http) {		
 	
