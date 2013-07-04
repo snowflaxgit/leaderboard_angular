108202a6bab95924c93fc785194ccead1e572608
snowflaxgit <android@snowflax.com>
2013-07-04 19:49:12 +0530

    Remove 404
    Not added

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
-	res.status(404);
+
 });
 // development only
 if ('development' == app.get('env')) {
